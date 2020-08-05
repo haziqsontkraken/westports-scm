36419369060420b9b13a2b1eb2524ffcfcd5b4bb
haziqsontkraken <69234466+haziqsontkraken@users.noreply.github.com>
2020-08-05 16:10:50 +0800

    Create IA05.php

---
+++ IA05.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+    <title></title>
+    <!-- jquery  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css"
+        integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+
+</head>
+
+<body>
+    <br>
+    <div class="container">
+        
+    </div>
+    <script>
+        //script to run when page loads
+       $(document).ready(function(){
+
+       });
+    </script>
+</body>
+
+</html>
